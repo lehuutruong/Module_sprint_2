3ae1affd1b45f69bfe39d95f18496355c9134246
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2023-01-10 20:24:09 +0700

    TruongLH serculity

---
+++ back_end_sprint_2/back_end_sprint_2/back_end_sprint_2/back_end_sprint_2/src/main/java/vn/sprint_2/controller/HomeRestController.java
@@ -0,0 +1,21 @@
+package vn.sprint_2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import vn.sprint_2.model.product.Category;
+import java.util.List;
+
+@CrossOrigin("*")
+@RequestMapping("/api/home")
+@RestController
+public class HomeRestController {
+
+//    @GetMapping()
+//    public ResponseEntity<?> list(){
+//        return
+//    }
+
+}
